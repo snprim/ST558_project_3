db1a640b44ce15418172c6733d226c93da420bba
snprim <sprim@ncsu.edu>
2020-11-01 01:00:39 -0500

    added DT::

--- breastCancer/ui.R
+++ breastCancer/ui.R
@@ -53,7 +53,7 @@ ui <- dashboardPage(
                                                checkboxGroupInput("varz", "Select Varaibles", choices = list("test", "test2")))),
                                     column(10,
                                            box(width = 12,
-                                               DT::dataTableOutput("tab"))
+                                               dataTableOutput("tab"))
                                            )
                                 )
                             )
